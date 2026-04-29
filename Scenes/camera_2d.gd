ee4cb901da3edee314df21c419f1e0ab4e2a0513
maskenary <125152643+maskenary@users.noreply.github.com>
2026-04-28 17:42:03 -0700

    grid snapping camera

---
+++ Scenes/camera_2d.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+var grid_size = Vector2(1920, 1080)
+@onready var parent = get_parent()
+	
+func _physics_process(delta: float) -> void:
+	var x = floor(parent.global_position.x / grid_size.x)
+	var y = floor(parent.global_position.y / grid_size.y)
+	global_position = Vector2(x * grid_size.x, y * grid_size.y)
